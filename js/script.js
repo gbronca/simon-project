575ec72a5fe32167d808d67245121c294ac96dcc
gbronca <gbronca@gmail.com>
2019-09-30 21:49:58 +0100

    Added start button event listener to initiate game

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 const colors = [];
+let power = false;
+let timerVariable;
+
+const counter = document.querySelector('.game-counter');
+const powerButton = document.querySelector('#power');
+const startButton = document.querySelector('#start');
 
 // Capture the ID names of the game colors and save it in the colors array
 const queryColors = document.getElementById('colors');
@@ -11,3 +17,17 @@ function playColor(color) {
 	const playedColor = document.querySelector(`div[id="${color}"]`);
 	playedColor.classList.add('playing');
 }
+
+function playAudio(color) {
+	const beep = document.querySelector(`audio[data-key="${color}"]`);
+	beep.play();
+	playColor(color);
+}
+
+startButton.addEventListener('click', () => {
+	if (power) {
+		startButton.innerHTML = 'Reset';
+		start();
+	}
+});
+  
